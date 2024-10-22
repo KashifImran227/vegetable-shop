711fb70ebdb551ac91929be62d5e9b16b752642b
KashifImran227 <164788718+KashifImran227@users.noreply.github.com>
2024-10-23 02:18:12 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar: React.FC = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 bg-transparent backdrop-blur-lg p-1 z-0 shadow-md transition-transform duration-300 ${
+      className={`fixed top-0 left-0 right-0 bg-transparent backdrop-blur-lg p-2 z-10 shadow-md transition-transform duration-300 ${
         navbarVisible ? "translate-y-0" : "-translate-y-full"
       }`}
     >
@@ -50,42 +50,17 @@ const Navbar: React.FC = () => {
             isOpen ? "block" : "hidden"
           } md:flex md:items-center md:space-x-6 md:static absolute bg-[#00000075] md:bg-transparent left-0 right-0 top-full transition-all duration-300 ease-in-out`}
         >
-          <li>
-            <Link
-              href="/"
-              className="text-white hover:text-green-300 transition duration-300 block p-4"
-              onClick={() => setIsOpen(false)} // Close menu on click
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/Products"
-              className="text-white hover:text-green-300 transition duration-300 block p-4"
-              onClick={() => setIsOpen(false)}
-            >
-              Products
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/About"
-              className="text-white hover:text-green-300 transition duration-300 block p-4"
-              onClick={() => setIsOpen(false)}
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/Contact"
-              className="text-white hover:text-green-300 transition duration-300 block p-4"
-              onClick={() => setIsOpen(false)}
-            >
-              Contact
-            </Link>
-          </li>
+          {["Home", "Products", "About", "Contact"].map((item) => (
+            <li key={item}>
+              <Link
+                href={`/${item === "Home" ? "" : item}`}
+                className="text-white hover:text-green-300 transition duration-300 block p-4"
+                onClick={() => setIsOpen(false)} 
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
