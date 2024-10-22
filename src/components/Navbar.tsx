137e55e25a96c24320774c2d453aa57aaca6f3e4
KashifImran227 <164788718+KashifImran227@users.noreply.github.com>
2024-10-23 02:47:58 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar: React.FC = () => {
   const [navbarVisible, setNavbarVisible] = useState(true);
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
+    setIsOpen((prev) => !prev);
   };
 
   const handleScroll = () => {
@@ -26,7 +26,7 @@ const Navbar: React.FC = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 bg-transparent backdrop-blur-lg p-2 z-10 shadow-md transition-transform duration-300 ${
+      className={`fixed top-0 left-0 right-0 bg-transparent backdrop-blur-lg p-1 z-50 shadow-md transition-transform duration-300 ${
         navbarVisible ? "translate-y-0" : "-translate-y-full"
       }`}
     >
@@ -48,14 +48,14 @@ const Navbar: React.FC = () => {
         <ul
           className={`${
             isOpen ? "block" : "hidden"
-          } md:flex md:items-center md:space-x-6 md:static absolute bg-[#00000075] md:bg-transparent left-0 right-0 top-full transition-all duration-300 ease-in-out`}
+          } md:flex md:items-center md:space-x-6 absolute md:static bg-[#00000075] md:bg-transparent left-0 right-0 top-full transition-all duration-300 ease-in-out`}
         >
           {["Home", "Products", "About", "Contact"].map((item) => (
-            <li key={item}>
+            <li key={item} className="border-b md:border-none">
               <Link
                 href={`/${item === "Home" ? "" : item}`}
                 className="text-white hover:text-green-300 transition duration-300 block p-4"
-                onClick={() => setIsOpen(false)} 
+                onClick={() => setIsOpen(false)} // Close menu on click
               >
                 {item}
               </Link>
